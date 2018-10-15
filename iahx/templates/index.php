340755262cc723940d82b039c4f888e74ad6a0ab
Jamil Atta Junior <atta.jamil@gmail.com>
2018-10-15 14:22:54 -0300

    Adiciona os arquivos .html

--- iahx/templates/index.php
+++ iahx/templates/index.php
@@ -1,1 +1,13 @@
-../../../iahx-opac/index.php
+<?php
+
+require_once 'environment.php';
+
+// include all views created in views path
+foreach(glob(VIEWS_PATH . "*.php") as $file) {
+    require_once $file;
+}
+
+// running the framework
+$app->run();
+
+?>
