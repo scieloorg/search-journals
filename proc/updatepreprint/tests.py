33e5d71d0855d7b0e3aa2810514ca0278ae8d27b
Jamil Atta Junior <atta.jamil@gmail.com>
2020-04-08 09:16:59 -0300

    Ajustes nos teste

--- proc/updatepreprint/tests.py
+++ proc/updatepreprint/tests.py
@@ -1,14 +1,15 @@
 # coding: utf-8
 import unittest
 from lxml import etree as ET
-import json
-import os
 
 import pipeline_xml
 
 
-namespaces = {}
-namespaces['dc'] = 'http://www.openarchives.org/OAI/2.0/provenance'
+namespaces = {'dc': 'http://purl.org/dc/elements/1.1/',
+              'xmlns': 'http://www.openarchives.org/OAI/2.0/',
+              'xsi': 'http://www.w3.org/2001/XMLSchema-instance',
+              'oai': 'http://www.openarchives.org/OAI/2.0/'}
+
 
 for namespace_id, namespace_link in namespaces.items():
     ET.register_namespace(namespace_id, namespace_link)
@@ -25,14 +26,21 @@ class TestAuthors(unittest.TestCase):
     def test_transform(self):
         text = """<root xmlns:dc="http://www.openarchives.org/OAI/2.0/provenance">
         <record>
-        <metadata>
-            <dc:creator>Trentin,Robson Gonçalves</dc:creator>
-            <dc:creator>Modolo,Alcir José</dc:creator>
-            <dc:creator>Vargas,Thiago de Oliveira</dc:creator>
-            <dc:creator>Campos,José Ricardo da Rocha</dc:creator>
-            <dc:creator>Adami,Paulo Fernando</dc:creator>
-            <dc:creator>Baesso,Murilo Mesquita</dc:creator>
-        </metadata>
+            <metadata>
+                <oai_dc:dc
+                    xmlns:oai_dc="http://www.openarchives.org/OAI/2.0/oai_dc/"
+                    xmlns:dc="http://purl.org/dc/elements/1.1/"
+                    xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+                    xsi:schemaLocation="http://www.openarchives.org/OAI/2.0/oai_dc/
+                    http://www.openarchives.org/OAI/2.0/oai_dc.xsd">
+                    <dc:creator>Trentin,Robson Gonçalves</dc:creator>
+                    <dc:creator>Modolo,Alcir José</dc:creator>
+                    <dc:creator>Vargas,Thiago de Oliveira</dc:creator>
+                    <dc:creator>Campos,José Ricardo da Rocha</dc:creator>
+                    <dc:creator>Adami,Paulo Fernando</dc:creator>
+                    <dc:creator>Baesso,Murilo Mesquita</dc:creator>
+                </oai_dc:dc>
+            </metadata>
         </record>
         </root>
         """
@@ -58,12 +66,19 @@ class TestTitles(unittest.TestCase):
     def test_transform(self):
         text = """<root xmlns:dc="http://www.openarchives.org/OAI/2.0/provenance">
         <record>
-        <metadata>
-            <dc:title xml:lang="en-US">COVID-19 in Brazil: advantages of a socialized unified health system and preparation to contain cases</dc:title>
-            <dc:title xml:lang="es-ES">COVID-19 in Brazil: advantages of a socialized unified health system and preparation to contain cases</dc:title>
-            <dc:title xml:lang="pt-BR">COVID-19 in Brazil: advantages of a socialized unified health system and preparation to contain cases</dc:title>
-            <dc:title xml:lang="fr-FR">COVID-19 in Brazil: advantages of a socialized unified health system and preparation to contain cases</dc:title>
-        </metadata>
+            <metadata>
+                <oai_dc:dc
+                    xmlns:oai_dc="http://www.openarchives.org/OAI/2.0/oai_dc/"
+                    xmlns:dc="http://purl.org/dc/elements/1.1/"
+                    xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+                    xsi:schemaLocation="http://www.openarchives.org/OAI/2.0/oai_dc/
+                    http://www.openarchives.org/OAI/2.0/oai_dc.xsd">
+                    <dc:title xml:lang="en-US">COVID-19 in Brazil: advantages of a socialized unified health system and preparation to contain cases</dc:title>
+                    <dc:title xml:lang="es-ES">COVID-19 in Brazil: advantages of a socialized unified health system and preparation to contain cases</dc:title>
+                    <dc:title xml:lang="pt-BR">COVID-19 in Brazil: advantages of a socialized unified health system and preparation to contain cases</dc:title>
+                    <dc:title xml:lang="fr-FR">COVID-19 in Brazil: advantages of a socialized unified health system and preparation to contain cases</dc:title>
+                </oai_dc:dc>
+            </metadata>
         </record>
         </root>
         """
