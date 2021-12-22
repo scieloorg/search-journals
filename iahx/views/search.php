9422d5ae70d4e15f31d693143e72f6fe3e1f1a07
Innolabs <atta.jamil@innolabs.com.br>
2021-12-22 02:18:17 -0300

    Remove retorno em formato XML e adiciona data no nome do arquivo de exportação

--- iahx/views/search.php
+++ iahx/views/search.php
@@ -389,9 +389,9 @@ $app->match('/', function (Request $request) use ($app, $DEFAULT_PARAMS, $config
     // output
     switch($output) {
 
-        case "xml": case "sol":
-            return new Response($dia_response, 200, array("Content-type" => "text/xml"));
-            break;
+        // case "xml": case "sol":
+        //     return new Response($dia_response, 200, array("Content-type" => "text/xml"));
+        //     break;
 
         case "print":
             return $app['twig']->render('print.html', $output_array);
@@ -429,7 +429,7 @@ $app->match('/', function (Request $request) use ($app, $DEFAULT_PARAMS, $config
         case "csv":
             if ( !isset($export_template) ){
                 $export_template = 'export-csv.txt';
-                $export_filename = 'export.csv';
+                $export_filename = 'export_' . date("Ymd") . '.csv';
                 $content_type = 'text/csv';
             }
             if ( !isset($export_total) ){
